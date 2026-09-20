8258d829079582ab1e4c54f7be1c0f692affa839
WitchDevelops <d.n.wojewska@gmail.com>
2026-09-21 00:35:49 +0200

    #11 style: remove fieldset border, add calculator cards grid

--- frontend/src/features/calculators/Calculators.tsx
+++ frontend/src/features/calculators/Calculators.tsx
@@ -6,26 +6,34 @@ import './Calculators.css';
 
 export const Calculators = () => {
 	return (
-		<div className="calculators__card">
-			<h2 className="card__title">Diluting stock</h2>
-			<form className="calculators__form">
-				<div className="form__body">
-					<Fieldset legend="Stock concentration">
-						<QuantityField name="stockConc" unitOptions={CONCENTRATION_UNITS} />
-					</Fieldset>
-					<Fieldset legend="Working concentration">
-						<QuantityField name="finalConc" unitOptions={CONCENTRATION_UNITS} />
-					</Fieldset>
-					<Fieldset legend="Final volume">
-						<QuantityField name="finalVolume" unitOptions={VOLUME_UNITS} />
-					</Fieldset>
-				</div>
-				<div className="form__submit">
-					<Button className="submit__button" type="submit">
-						Calculate
-					</Button>
-				</div>
-			</form>
+		<div className="calculators__container">
+			<div className="calculators__card">
+				<h2 className="card__title">Diluting stock</h2>
+				<form className="calculators__form">
+					<div className="form__body">
+						<Fieldset legend="Stock concentration">
+							<QuantityField
+								name="stockConc"
+								unitOptions={CONCENTRATION_UNITS}
+							/>
+						</Fieldset>
+						<Fieldset legend="Working concentration">
+							<QuantityField
+								name="finalConc"
+								unitOptions={CONCENTRATION_UNITS}
+							/>
+						</Fieldset>
+						<Fieldset legend="Final volume">
+							<QuantityField name="finalVolume" unitOptions={VOLUME_UNITS} />
+						</Fieldset>
+					</div>
+					<div className="form__submit">
+						<Button className="submit__button" type="submit">
+							Calculate
+						</Button>
+					</div>
+				</form>
+			</div>
 		</div>
 	);
 };

0a9eddaf85396bbf7bd61490f73d3e8f88c89760
WitchDevelops <d.n.wojewska@gmail.com>
2026-09-21 00:42:05 +0200

    #11 fix: zod schema to accept values form the form

--- frontend/src/features/calculators/dilution/dilutionSchema.ts
+++ frontend/src/features/calculators/dilution/dilutionSchema.ts
@@ -2,11 +2,11 @@ import { z } from 'zod';
 
 export const dilutionInputSchema = z
 	.object({
-		stockConc: z.coerce.number().positive(),
-		finalConc: z.coerce.number().positive(),
-		finalVolume: z.coerce.number().positive(),
+		stockConcValue: z.coerce.number().positive(),
+		finalConcValue: z.coerce.number().positive(),
+		finalVolumeValue: z.coerce.number().positive(),
 	})
-	.refine((data) => data.finalConc < data.stockConc, {
+	.refine((data) => data.finalConcValue < data.stockConcValue, {
 		path: ['finalConc'],
 		message:
 			'Final concentration cannot be higer than the stock concentration.',

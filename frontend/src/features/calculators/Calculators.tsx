0a7c7bcb3ce4168cdb7d3675464f5f53439c7296
WitchDevelops <d.n.wojewska@gmail.com>
2026-09-22 23:15:25 +0200

    #27 refactor(frontend): extract Card into a separate reusable component

--- frontend/src/features/calculators/Calculators.tsx
+++ frontend/src/features/calculators/Calculators.tsx
@@ -1,13 +1,13 @@
+import { Card } from '../../components/molecules/Card';
 import { DilutionForm } from './dilution/DilutionForm';
 import './Calculators.css';
 
 export const Calculators = () => {
 	return (
 		<div className="calculators__container">
-			<div className="calculators__card">
-				<h2 className="card__title">Diluting stock</h2>
+			<Card title="Diluting stock">
 				<DilutionForm />
-			</div>
+			</Card>
 		</div>
 	);
 };
